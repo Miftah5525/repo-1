3e78579364d98e612094fdb65ad055c01e6f2156
Miftah5525 <miftahfentaw186@gmail.com>
2025-03-21 00:59:28 +0300

    commiting a new dart file

---
+++ main.dart
@@ -0,0 +1,74 @@
+// OOP CONCEPTS IN DART PROGRAMING LANGUAGE 
+
+
+// FIRST LEVEL BASICS
+
+
+
+
+   /*
+   //class declaration
+
+
+class students{
+// attributes declaration
+  String? name;
+  int? age;
+  String? sex;
+
+
+
+void show(){
+  print("name is $name \nage is $age \nsex is $sex");  //method declaration
+}
+
+}
+
+
+void main(){
+  students Miftah = students();  // object declaration 
+  Miftah.name = "miftah";
+  Miftah.age = 23;
+  Miftah.sex = "male";
+Miftah.show();
+}
+
+*/
+
+
+
+
+/*
+// CLASSES WITH CONSTRUCTORS
+import 'dart:io';
+
+
+class cbe{
+
+  String? account_hoder;
+  String? balance;
+  
+
+  // cbe(this.account_hoder,this.balance){
+  //   this.account_hoder = account_hoder;
+  //   this.balance = balance;
+  }
+
+
+
+  void register_user(){
+    stdout.write("Enter the acount holder name: ");
+    String? account_hoder = stdin.readLineSync();
+    stdout.write("Enter the opening account balance: ");
+    String? balance =stdin.readLineSync();    
+
+    print("account holder = $account_hoder \ninitial balance = $balance");
+
+  }
+
+
+void main(){
+  cbe miftah = cbe();
+  register_user();
+}
+*/
