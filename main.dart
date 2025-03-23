36ffffeff603e9c6cd5237a44d8e4df948cae21b
Miftah5525 <miftahfentaw186@gmail.com>
2025-03-23 22:58:31 +0300

    a basic error handling code is added to the file

--- main.dart
+++ main.dart
@@ -87,67 +87,127 @@ Miftah.show();
 
 // TASK ONE 
 
-import 'dart:io';
+// import 'dart:io';
 
-class Employees{
-  String name;
-  String gender;
-  String position;
-  int age;
-  double salary;
-
-
-
-Employees(this.name,this.age,this.gender,this.position,this.salary);
+// class Employees{
+//   String name;
+//   String gender;
+//   String position;
+//   int age;
+//   double salary;
 
 
-Employees.intern(String name)
-  : this.name = name,
-    gender = "unknown",
-    position = "intern",
-    age = 25,
-    salary = 1000.0 {
-  // Constructor body can be empty or include initialization logic if needed
-}
 
-Employees.high_salary(this.name,this.age,this.gender,this.position,this.salary) {
-  if (salary < 5000.0){
-    print("too low salaty to register");
-    exitCode;
-  }
-  else{
-     this.salary = salary;
-     this.age = age;
-     this.gender = gender;
-     this.position = position;
-     this.name = name;
-  print("salary level is acceptable and the employeee information is");
-  print("name = $name");
-   print("age = $age");
-    print("gender = $gender");
-     print("position = $position");
-      print("salary = $salary");
-  }
-}
+// Employees(this.name,this.age,this.gender,this.position,this.salary);
 
-void display_datas(){
-  print("name of employe = $name\nage of employe = $age\ngender = $gender\nsalary = $salary\nposition = $position");
-}
+
+// Employees.intern(String name)
+//   : this.name = name,
+//     gender = "unknown",
+//     position = "intern",
+//     age = 25,
+//     salary = 1000.0 {
+//   // Constructor body can be empty or include initialization logic if needed
+// }
+
+// Employees.high_salary(this.name,this.age,this.gender,this.position,this.salary) {
+//   if (salary < 5000.0){
+//     print("too low salaty to register");
+//     exitCode;
+//   }
+//   else{
+//      this.salary = salary;
+//      this.age = age;
+//      this.gender = gender;
+//      this.position = position;
+//      this.name = name;
+//   print("salary level is acceptable and the employeee information is");
+//   print("name = $name");
+//    print("age = $age");
+//     print("gender = $gender");
+//      print("position = $position");
+//       print("salary = $salary");
+//   }
+// }
+
+// void display_datas(){
+//   print("name of employe = $name\nage of employe = $age\ngender = $gender\nsalary = $salary\nposition = $position");
+// }
   
 
+// }
+
+// void main(){
+
+// Employees helen = Employees.intern("helen");
+// Employees meki = Employees("meki", 30, "male", "SENG", 8000.0);
+// helen.display_datas();
+
+// print("\n\n\n");
+// Employees sami =Employees.high_salary("sami", 24, "male", "analyst", 6000.0);
+
+
+
+
+  
+//   }
+
+
+
+// ERROR HANDLING IN DART 
+// USING TRY AND CATCH 
+
+
+import "dart:io";
+
+class BankingSystem{
+
+
+
+void Banker(){
+
+  stdout.writeln("welcome to miftah commercial banking(MCB) ");
+  stdout.writeln("enter the amount of initial balance ");
+  String? initial_balance_input = stdin.readLineSync();
+
+  
+  stdout.writeln("enter the amount of withdrawal ");
+  String? withdrawal_amount_input = stdin.readLineSync();
+
+try{
+
+if(initial_balance_input != null && withdrawal_amount_input != null){
+
+      int initial_balance = int.parse(initial_balance_input);
+      int withdrawal_amount = int.parse(withdrawal_amount_input);
+       print("your initial balance is $initial_balance\nand your withdrawal request is $withdrawal_amount");
+
+       if(withdrawal_amount < initial_balance){
+
+        // print(initial_balance - withdrawal_amount);
+        print("you made a withdrawal of $withdrawal_amount and your initial balance is now ${initial_balance - withdrawal_amount} \nthank you for choosing MCB");
+
+       }else{
+        print("unable to do the transaction ,you withdrawal request is larger that your initial balance.");
+       
+       }
+    }
+
+}catch (e){
+  if(initial_balance_input != null && withdrawal_amount_input != null){
+    print("null input is not suppoted, please change your input and rerun the program");
+  }
+}
+
+}
+  
 }
 
 void main(){
-
-Employees helen = Employees.intern("helen");
-Employees meki = Employees("meki", 30, "male", "SENG", 8000.0);
-helen.display_datas();
-
-print("\n\n\n");
-Employees sami =Employees.high_salary("sami", 24, "male", "analyst", 6000.0);
+var transaction1 = BankingSystem();
+transaction1.Banker();
+}
 
 
 
 
-  
-  }
